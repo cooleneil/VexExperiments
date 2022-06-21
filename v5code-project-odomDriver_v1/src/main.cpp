25572d270f061d31b6267af28001ab43731bfc7e
cooleneil <106409342+cooleneil@users.noreply.github.com>
2022-06-21 13:44:20 -0700

    Working(3Encoders)

--- v5code-project-odomDriver_v1/src/main.cpp
+++ v5code-project-odomDriver_v1/src/main.cpp
@@ -6,73 +6,7 @@
 // Controller1          controller                    
 // Drivetrain           drivetrain    1, 2            
 // ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       G, H            
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 2            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 2            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// Controller1          controller                    
-// Drivetrain           drivetrain    1, 2            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// Drivetrain           drivetrain    1, 2            
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// Drivetrain           drivetrain    1, 2            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// front_R              motor         1               
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// front_L              motor         2               
-// front_R              motor         1               
-// leftEncoder          encoder       C, D            
-// rightEncoder         encoder       E, F            
-// ---- END VEXCODE CONFIGURED DEVICES ----
+
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -135,124 +69,7 @@ void autonomous(void) {
   // ..........................................................................
   // Insert autonomous user code here.
   // ..................................................................
-  // ,lljjlllljk777........ leftside.setVelocity(100, pct);
-  // rightside.setVelocity(100, pct);
-
-  // f_bar_a.spinFor(fwd, -20, deg, false);
-
-  // f_bar_b.spinFor(fwd, 20, deg);
-  // f_bar_a.stop(hold);
-  // f_bar_b.stop(hold);
-  // clamp.spinFor(reverse, -385, deg, false);
-
-  // leftside.spinFor(fwd, 1265, deg, false);
-  // rightside.spinFor(fwd, -1265, deg);
-  // leftside.stop(hold);
-  // rightside.stop(hold);
-  /*leftside.spinFor(fwd, 1320, deg, false);
-  rightside.spinFor(fwd, -1320, deg);
-  leftside.stop(hold);
-  rightside.stop(hold);*/
-
-  /*float currentPosition = ((back_L.rotation(deg) + back_R.rotation(deg) +
- front_L.rotation(deg) + front_R.rotation(deg)) / 4); float Target = 1320; float
- error = Target - currentPosition;
-
- float k = -5;
-  while (error < Target) {
-    currentPosition = ((back_L.rotation(deg) + back_R.rotation(deg) +
- front_L.rotation(deg) + front_R.rotation(deg)) / 4); error = Target -
- currentPosition;
-
-    rightside.spin(reverse, (error * k), volt);
-    leftside.spin(fwd, (error * k), volt);
-    wait(20, msec);  }*/
-
-  // wait(200, msec);
-
-  // clamp.spinFor(fwd, -220, deg, false);
-  // wait(1250, msec);
-  // Back Up
-  // rightside.spinFor(fwd, 350, deg);
-  // rightside.stop(hold);
-  // leftside.setVelocity(80, pct);
-
-  // Back up
-  // leftside.spinFor(reverse, 780, deg, false);
-  // rightside.spinFor(reverse, -780, deg);
-  // leftside.setVelocity(30, pct);
-  // rightside.setVelocity(30, pct);
-  // clamp.spinFor(fwd, 200, deg);
-
-  // leftside.spinFor(reverse, 600, deg, false);
-  // rightside.spinFor(reverse, -600, deg);
-
-  // leftside.spinFor(reverse, 117, deg, false);
-  // rightside.spinFor(fwd, -117, deg);
-  // leftside.spinFor(fwd, 1570, deg, false);
-  // //1480
-  // rightside.spinFor(fwd, -1570, deg);
-  // //leftside.stop(hold);
-  // //rightside.stop(hold);
-  // /*leftside.setVelocity(70, pct);
-  // rightside.setVelocity(70, pct);
-  // leftside.spinFor(fwd, 185, deg, false);
-  // rightside.spinFor(fwd, -185, deg);*/
-
-  // clamp.spinFor(fwd, -250, deg, false);
-  // wait(750, msec);
-
-  // // Back Up
-  // //rightside.spinFor(fwd, 350, deg);
-  // //rightside.stop(hold);l;]\
-  // //leftside.setVelocity(80, pct);
-  // leftside.setVelocity(80, pct);
-  // rightside.setVelocity(80, pct);
-  // //Back up
-  // leftside.spinFor(reverse, 1100, deg, false);
-  // rightside.spinFor(reverse, -1100, deg);
-  // wait(750, msec);
-  // Rigght
-  /*rightside.spinFor(fwd, 650, deg);
-  clamp.spinFor(fwd, -150, deg);
-
-  leftside.spinFor(fwd, -430, deg, false);
-  rightside.spinFor(fwd, 430, deg);
-  //wait(1000, msec);
-  rightside.spinFor(fwd, -900, deg);
-  leftside.spinFor(fwd, 755, deg, false);
-  rightside.spinFor(fwd, -755, deg);
-
-  leftside.setVelocity(70, pct);
-  rightside.setVelocity(70, pct);
-  leftside.spinFor(fwd, 45, deg, false);
-  rightside.spinFor(fwd, -45, deg);
-
-
-  clamp.spinFor(fwd, 150, deg);
-
-  leftside.spinFor(fwd, -1100, deg, false);
-  rightside.spinFor(fwd, 1100, deg);
-  //Left
-  //leftside.spinFor(fwd, 1200, deg);
-
-  //
-  rightside.spinFor(fwd, 4000, deg);
-
-  leftside.spinFor(fwd, 400, deg, false);
-  rightside.spinFor(fwd, -400, deg);
-
-  leftside.spinFor(fwd, 800, deg, false);
-  rightside.spinFor(fwd, -800, deg);
-
-  clamp.spinFor(fwd, -150, deg);
-
-  leftside.spinFor(reverse, 800, deg, false);
-  rightside.spinFor(reverse, -800, deg);
-  rightside.setVelocity(100, pct);
-  leftside.setVelocity(100, pct);*/
-
-  // wait(20, msec);
+ 
 }
 
 /*---------------------------------------------------------------------------*/
@@ -289,35 +106,7 @@ void usercontrol(void) {
 
     //   front_L.spin(fwd, leftmotorpower, pct);
     //   front_R.spin(fwd, -rightmotorpower, pct);
-      //       while (true) {
-      //         float ydisL = 3;//encoder values
-      //         float ydisR = 3;//encoder values
-      //         float Xdis = 4;//encoder values
-      //         float inertialAngle = 60;//heading
-      //         bool Fwd = true;
-      //         float yPos = 0;//
-      //         float xPos = 0;
-      //         float X_Position = 0;
-      //         float Y_Position = 0;
-      //         if (inertialAngle == 0) {
-      //           if (Fwd == true) {
-      //             yPos == (ydisL + ydisR) / 2;
-      //             xPos == Xdis;
-      //           } if(inertialAngle == 0) {
-      //             if (Fwd == false){
-      //             yPos == (ydisL + ydisR) / -2;
-      //             xPos == -1 * Xdis;
-      //             }
-      //           }
-      //         }
-      //         if( inertialAngle != 0 && (ydisR + ydisL) > 0 ){
-      // float yDisTrav = (ydisR + ydisL)/2 ;
-      // xPos ==  (double (cos(inertialAngle)))*yDisTrav;
-      // yPos == (double (sin(inertialAngle)))*xPos;
-      //         }
-      // X_Position = X_Position + xPos;
-      // Y_Position = Y_Position + yPos;
-
+      
 
 
 double L= 0;
@@ -352,7 +141,7 @@ while (true) {
 
         // position
         if(headingAngleDelta != 90){
-        yPos = double((cos(headingAngle))) * CDis;
+          
         }
         else {
          // yPos = 3.00;
@@ -420,6 +209,6 @@ while (true) {
 
     // Prevent main from exiting with an infinite loop.
     while (true) {
-      wait(1, msec);
+      wait(100, msec);
     }
   }
